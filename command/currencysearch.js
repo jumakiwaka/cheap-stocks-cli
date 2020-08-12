98c24473747a5e8fd8fd8f01ebf866808e18ac97
jumakiwaka <alijuma232@gmail.com>
2020-08-12 22:38:46 +0300

    add more tests for searching currency and util funcs

--- command/currencysearch.js
+++ command/currencysearch.js
@@ -1,40 +1,45 @@
+const axios = require('axios');
+const chalk = require('chalk');
+const util = require('../lib/util');
 
-const axios = require("axios");
-const util = require("../lib/util");
-const chalk = require("chalk");
+async function searchCurrency() {
+  try {
+    if (!process.argv[3]) {
+      throw new Error('Missing required arguments, [currency ISO code]');
+    }
+    const isoCode = process.argv[3].toUpperCase();
+    console.log(`searching currency ${isoCode}...`);
+    const options = {
+      method: 'get',
+      redirect: 'follow',
+    };
+    const url =
+      'https://focusmobile-interview-materials.s3.eu-west-3.amazonaws.com/Cheap.Stocks.Internationalization.Currencies.csv';
+    const res = await axios(url, options);
 
-async function searchCurrency(){
-	try{
-	if(!process.argv[3]){
-		throw new Error("Missing required arguments, [currency ISO code]");
-	}
-	const isoCode = process.argv[3].toUpperCase();
-	console.log(`searching currency ${isoCode}...`);
-	const options = {
-		method: 'get',
-		redirect: 'follow',
-	};
-	const url = 'https://focusmobile-interview-materials.s3.eu-west-3.amazonaws.com/Cheap.Stocks.Internationalization.Currencies.csv';
-	const res = await axios(url, options);
-
-	const {data} = res;
-	// get data rows without headers
-	const rows = data.split('\r\n').slice(1);
-	// get specific column data as an array
-	const columns = util.getCols(rows, 2); // currency ISO codes
-
-	// find if currency is available
-	const isAvailableCurrency = util.lookupCurrency(columns, isoCode);
-	if(isAvailableCurrency){
-		console.log(chalk.green(`hurray! ${isoCode} is supported in cheap stocks Inc.`));
-	} else{
-		console.log(chalk.yellow(`Oops! ${isoCode} is not yet supported in cheap stocks Inc.`));
-	}
-  return isAvailableCurrency;
-	}catch (error) {
-		util.handleError(error);
-	}
+    const { data } = res;
+    // get data rows without headers
+    const rows = data.split('\r\n').slice(1);
+    // get specific column data as an array
+    const columns = util.getCols(rows, 2); // currency ISO codes
+    // find if currency is available
+    const isAvailableCurrency = util.lookupCurrency(columns, isoCode);
+    if (isAvailableCurrency) {
+      console.log(
+        chalk.green(`hurray! ${isoCode} is supported in cheap stocks Inc.`)
+      );
+    } else {
+      console.log(
+        chalk.yellow(
+          `Oops! ${isoCode} is not yet supported in cheap stocks Inc.`
+        )
+      );
+    }
+    return isAvailableCurrency;
+  } catch (error) {
+    util.handleError(error);
+    return false;
+  }
 }
 
-module.exports = { searchCurrency, };
-
+module.exports = { searchCurrency };
--- test/searchCurr.Test.js
+++ test/searchCurr.Test.js
@@ -1,30 +1,80 @@
-const {expect} = require("chai");
-const {searchCurrency} = require("../command/currencysearch");
-const {getCols, handleError} = require("../lib/util");
+const { expect } = require('chai');
+const chalk = require('chalk');
+const { searchCurrency } = require('../command/currencysearch');
+const { getCols } = require('../lib/util');
 
-describe("Search currency",function () {
+describe('Searching currency', function () {
   this.timeout('25s');
-	describe("given correct arguments have been passed", () => {
+  describe('given correct arguments have been passed', () => {
     const consoles = [];
     before(() => {
       console.log = (msg) => {
-				consoles.push(msg);
-			}
-    process.argv = ["cs", "search", "currency", "lyd"]
+        consoles.push(msg);
+      };
+      process.argv = ['cs', 'search', 'currency', 'lyd'];
+    });
+    it('should look up currency and print support status to stdout', async () => {
+      const isSupported = await searchCurrency();
+      expect(isSupported).to.be.a('boolean');
+      expect(consoles[0]).to.equal('searching currency LYD...');
+      expect(consoles[1]).to.equal(
+        chalk.green('hurray! LYD is supported in cheap stocks Inc.')
+      );
+    });
+  });
+  describe('given currency is not supported yet', () => {
+    const consoles = [];
+    before(() => {
+      console.log = (msg) => {
+        consoles.push(msg);
+      };
+      process.argv = ['cs', 'search', 'currency', 'nzd'];
+    });
+    it('should look up currency and print support status to stdout', async () => {
+      const isSupported = await searchCurrency();
+      expect(isSupported).to.be.a('boolean');
+      expect(consoles[0]).to.equal('searching currency NZD...');
+      expect(consoles[1]).to.equal(
+        chalk.yellow('Oops! NZD is not yet supported in cheap stocks Inc.')
+      );
+    });
+  });
+
+  describe('given not supplied with the currency ISO code', () => {
+    const consoles = [];
+    before(() => {
+      console.error = (msg) => {
+        consoles.push(msg);
+      };
+      process.argv = ['cs', 'search', 'currency'];
+    });
+    it('should look up currency and print support status to stdout', async () => {
+      const isSupported = await searchCurrency();
+      expect(isSupported).to.be.a('boolean');
+      expect(consoles[0]).to.equal(
+        chalk.redBright(
+          'Error: Missing required arguments, [currency ISO code]'
+        )
+      );
+    });
+  });
 });
-		it("should look up currency and print support status to stdout",  async () => {
-				
-				const isSupported =  await searchCurrency();
-				expect(isSupported).to.be.a("boolean");
-				expect(consoles[0]).to.equal("searching currency LYD...");
-				expect(consoles[1]).to.equal('\u001b[32mhurray! LYD is supported in cheap stocks Inc.\u001b[39m');
-				
-		});
-	});
 
-});
-
-
-describe("Search utils", () => {
-
+describe('Search utils', () => {
+  describe('Getting a column', () => {
+    it('should parse the csv and return the desired column', () => {
+      const rows = [
+        'Algeria,Algerian dinar,DZD',
+        'Angola,Angolan kwanza,AOA',
+        'Benin,CFA franc,XOF',
+        'Botswana,Botswana pula,BWP',
+        'Burkina Faso,CFA franc,XOF',
+        'Burundi,Burundi franc,BIF',
+        'Cameroon,CFA franc,XAF',
+      ];
+      const col3 = getCols(rows, 2);
+      const expectedCol3 = ['DZD', 'AOA', 'XOF', 'BWP', 'XOF', 'BIF', 'XAF'];
+      expect(col3).to.deep.equal(expectedCol3);
+    });
+  });
 });
